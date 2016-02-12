f2aa32c1b4eff8a01657326e25def868c6026387
ploidt <ploid@Ploid-Macbook.local>
2016-02-12 17:41:47 +0700

    evrnt php

---
+++ event.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>Event</title>
+	<meta charset="utf-8">
+	<meta name = "format-detection" content = "telephone=no" />
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/touchTouch.css">
+	<link rel="stylesheet" href="css/event.css" type="text/css">
+	<!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+<!--	<link rel="stylesheet" href="css/museumhome.css" type="text/css">-->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/touchTouch.jquery.js"></script>
+	<script src="js/jquery-migrate-1.2.1.js"></script>
+	<script src="js/script.js"></script>
+	<script src="js/packery.pkgd.min.js"></script>
+	<!--[if (gt IE 9)|!(IE)]><!-->
+	<script src="js/jquery.mobile.customized.min.js"></script>
+	<script src="js/wow.js"></script>
+<!--
+	<script>
+		$(document).ready(function () {
+			if ($('html').hasClass('desktop')) {
+				new WOW().init();
+			}
+		});
+	</script>
+-->
+    </head>
+    
+    <body>    
+    <?php 
+        include("nav.inc.html");
+    ?>
+    <section id="content">
+	<div class="full-width-container block-1">
+		<div class="container">
+			<div class="row">
+				<div class="grid_12">
+					<header>
+						<h2><span>Event</span></h2>
+						<hr>
+					</header>
+				</div>
+				<div id="touch_gallery">
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/awake.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Awake</h4>
+							<ul>
+							    <li class="content">Date: Sat, March 26  - Mon, March 14</li>
+							    <li class="content">Time: 10:00am - 4:00pm</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/20century.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Twenty Century Print</h4>
+							<ul>
+							    <li class="content">Date: Wed, February 24  - Thu, April 21</li>
+							    <li class="content">Time: 10:00am - 7:00pm</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/ddld.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Dusadee + Dusit + Lovers + Dead Ones</h4>
+							<ul>
+							    <li class="content">Date: Tue, February 16  - Sat, April 9</li>
+							    <li class="content">Time: 10:00am - 6:00pm</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/riddle.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Riddle Shadow</h4>
+							<ul>
+							    <li class="content">Date: Sat, May 23  - Thu, June 18</li>
+							    <li class="content">Time: 10:00am - 7:00pm</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/adn.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Art  Dharma Naturally</h4>
+							<ul>
+							    <li class="content">Date: Fri, March 20  - Tue, April 21</li>
+							    <li class="content">Time: 10:00am - 7:00pm</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-md-4">
+						<div class="img_block"><img class="img-pic" src="img/event/opensecret.jpg" alt="Event"></div>
+						<article class="eventbox">
+							<h4 class="maintext">Open Secret</h4>
+							<ul>
+							    <li class="content">Date: Fri, February 27 - Fri, April 10</li>
+							    <li class="content">Time: 12:00am - 12:00am</li>
+							    <li class="content">Organizer: Art Exibition</li>
+							    <li class="content">Event Type: Art/Music/Cultural Event</li>
+							    <li class="content">Conducted in: Thai</li>
+							    <li><div class="reservebu">RESERVE</div></li>
+							</ul>
+						</article>
+					</div>
+			    </div>
+		    </div>
+        </div>
+	</div>
+</section>
+
+    </body>
+</html>
