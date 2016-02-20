d6e0869d30c1d3963828c0da42b10a711eb9e419
ploidt <ploid@Ploid-Macbook.local>
2016-02-20 21:45:14 +0700

    ress quiz

--- js/quiz.js
+++ js/quiz.js
@@ -50,13 +50,13 @@
         
         $scope.createShareLinks = function(percentage){
 			
-			var url = 'http://chowtime.com';
+			var url = 'http://quiz.chowtime.com';
 
 //            var emailLink = '<a class="btn email" href="mailto:?&subject=Try to beat my score!&body=I%20scored%20a%20'+percentage+'%25%20on%20this%20quiz.%20Try%20to%20beat%20my%20score%20at%20'+url+'">Send Email</a>';
 
 			var twitterLink = '<a target="blank" class="btn twitter" href="http://twitter.com/share?text=I scored a '+percentage+'%25 on this quiz. Try to beat my score at&amp;hashtags=ChowTimeQuizQuiz&amp;url='+url+'">Tweet your score</a>';
             
-            var emailLink = '<a target="blank" class="btn twitter" href="https://www.facebook.com/sharer/sharer.php?u=www.chowtime.com">Share on Facebook</a>';
+            var emailLink = '<a target="blank" class="btn fb" href="https://www.facebook.com/sharer/sharer.php?u=www.chowtime.com">Share on Facebook</a>';
 			
 			var newMarkup = emailLink + twitterLink;
 
