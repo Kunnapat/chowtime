e29585a9ba75f0ec084b4c654459a219b3828efb
june38 <sutattajune@hotmail.com>
2016-03-30 10:42:15 +0700

    register ได้หมดแล้ว
    ปล.ถ้า registration.php ใช้ไม่ได้มาบอกด้วย5555จะอัพหม่ให้

--- registersave.php
+++ registersave.php
@@ -1,8 +1,49 @@
 <html>
 <body>
     <?php
+        if("" == trim($_POST['username'])){
+            $errmsg = "please fill in username";
+        }else if("" == trim($_POST['question'])){
+            $errmsg = "please fill in security question";
+        }else if("" == trim($_POST['answer'])){
+            $errmsg = "please fill in security answer";
+        }else if("" == trim($_POST['fname'])){
+            $errmsg = "please fill in first name";
+        }else if("" == trim($_POST['lname'])){
+            $errmsg = "please fill in last name";
+        }else if("" == trim($_POST['tel'])){
+            $errmsg = "please fill in phone number";
+        }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errmsg = "your email is incorrect format";
+        }else if("" == trim($_POST['password'])){
+            $errmsg = "please fill in password";
+        }else if("" == trim($_POST['repassword'])){
+            $errmsg = "please fill in confirm password";
+            
+        }
+        if("" != $_POST['password']){
+            $pw = $_POST['password'];
+        }
+        if("" != $_POST['repassword']){
+            $repw = $_POST['repassword'];
+            
+        }
+         if($pw != $repw){
+            $errmsg = "password and confirm password is not match";
+        }
     
-     
+//    if($objQuery){
+//        echo "<a href='./museumhome.php'>Register Complete Go BACK TO Homepage</a>";
+//    }else{
+//        echo "Error: " . $strSQL . "<br>" . $conn->error;
+//        
+////       echo "<font size=5 color=red>$errmsg<p />
+////         <a href=\"javascript: history.back()\">come back to edit</a></font>";
+//    }
+    if($errmsg != "") {
+    echo "<font size=5 color=red>$errmsg<p />
+         <a href=\"javascript: history.back()\">come back to edit</a></font>";
+    }else {
         $objConnect = mysql_connect("localhost","root","root") or die("Error Connect to Database");
         $objDB = mysql_select_db("chowtime");
         $strSQL = "INSERT INTO members ";
@@ -11,12 +52,7 @@
         $strSQL .="(0,'".$_POST["fname"]."','".$_POST["lname"]."','".$_POST["username"]."','".$_POST["password"]."','".$_POST["email"]."','".$_POST["tel"]."','".$_POST["gender"]."','".$_POST["birthdate"]."','1','".$_POST["question"]."','".$_POST["answer"]."'";
         $strSQL .=",'12345','','NULL') ";
         $objQuery = mysql_query($strSQL);
-    if($objQuery){
         echo "<a href='./museumhome.php'>Register Complete Go BACK TO Homepage</a>";
-    }else{
-        echo "Error: " . $strSQL . "<br>" . $conn->error;
-//       echo "<font size=5 color=red>$errmsg<p />
-//         <a href=\"javascript: history.back()\">come back to edit</a></font>";
     }
     mysql_close($objConnect);
     ?>
